04bc25dd65606a25b5f787c45ab374f50a4d1ace
alexfranco311 <alexfranco311@gmail.com>
2021-10-22 00:30:11 -0400

    completed inRange & words challenges

--- _.js
+++ _.js
@@ -1,21 +1,37 @@
 const _ = {
-    clamp(number, lower, upper){
+     clamp(number, lower, upper){
         const lowerClampedValue = Math.max(number, lower);
         const clampedValue = Math.min(lowerClampedValue, upper);
         return clampedValue;
-    },
-    inRange(number, start, end){
-        if(end == undefined){
-            return end = start, start = 0
-        } else if(start > end) {
-            return temp = end, start = temp, end = start
-        } return number
      },
+    inRange(number, start, end){
+        if(number === end){
+            return false
+        } else if(end === undefined){
+            end = start;
+            start = 0;
+            return true
+        } else if(start > end) {
+            temp = end; 
+            start = temp;
+            end = start;
+            return true
+        } else if(number < start || number > end){
+            return false
+        } else if(number >= start || number < end){
+            return true
+        }
+     },
+    words(string){
+        return string.split(' '); 
+     },
+    pad(string, length){
+        if(string.length > length){
+            return string
+        }
+    } 
+
 };
     
-
-
-
-
 // Do not write or modify code below this line.
 module.exports = _;
