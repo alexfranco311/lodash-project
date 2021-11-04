0101ee0542e2ed5813ee76463e7d3666e7be0864
alexfranco311 <alexfranco311@gmail.com>
2021-11-04 01:00:47 -0400

    progress 30/43

--- _.js
+++ _.js
@@ -26,11 +26,45 @@ const _ = {
         return string.split(' '); 
      },
     pad(string, length){
-        if(string.length > length){
+        if(length <= string.length){
             return string
-        }
-    } 
+        };
+        const startPaddingLength = Math.floor((length - string.length) / 2);
+        const endPaddingLength = length - string.length - startPaddingLength;
+        const paddedString = ' '.repeat(startPaddingLength) + string + ' '.repeat(endPaddingLength);
 
+            return paddedString;
+    },
+    has(object, key){
+        if(object[key] === undefined){
+            return false
+        };
+
+        for(const key in object){
+            if(object.hasOwnProperty(key)){
+                return true;
+            } 
+        };
+    },
+    invert(object){
+        let invObject = new Object;
+        for(let key in object){
+            const originalValue = object[key];
+            invObject[originalValue] = key;
+        } return invObject;
+    },
+    findKey(object, predicate){
+        for(let key in object){
+            let value = object[key];
+            let prediacateReturnValue = predicate(value);
+            if(prediacateReturnValue){
+                return key;
+            } else undefined
+        }
+    }, 
+    drop(){
+        
+    },
 };
     
 // Do not write or modify code below this line.
